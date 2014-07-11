727343c925d8210b092392d58d9d62fd0206852a
kbf09 <pexpex123@gmail.com>
2014-07-11 23:05:48 +0900

    aaa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,10 +20,16 @@ public class Controller implements Initializable{
 
     @FXML ScrollPane scrollPane;
     @FXML ListView listView;
+    @FXML TextField ipAddress;
 
     private Toolkit kit;
     private Clipboard clip;
     private ObservableList<String> listRecords;
+    private Socket sock;
+
+
+
+
     @Override
     public void initialize(final URL location, final ResourceBundle resources ) {
         kit = Toolkit.getDefaultToolkit();
@@ -48,6 +55,32 @@ public class Controller implements Initializable{
             e.printStackTrace();
         }
         listView.setItems(listRecords);
+    }
+
+    @FXML
+    public void startConnection() {
+        int port; // ポート番号
+        int timeout_msec = 10000; // accept() のタイムアウトは10秒にしてみる。
+
+        port = 50000; // 例えばポート番号は 50000 番にする。
+
+        try{
+            // サーバソケットの作成
+            ServerSocket svsock = new ServerSocket(port);
+            svsock.setSoTimeout(timeout_msec);  // ※必要があるときのみ設定する。
+
+            // クライアントからの接続を受け付ける。
+            // ※クライアントからの接続がくるまでここでブロックする。
+
+            Socket sock = svsock.accept();
+
+            // クライアントと接続されたソケットを利用して処理を行う。
+
+        } catch (Exception e) {
+            // accept() タイムアウト時の処理をする。
+
+        }
+        ipAddress.getText();
     }
 
 
